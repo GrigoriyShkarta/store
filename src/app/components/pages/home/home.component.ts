bc03e3f31a759ed3e20a8c342962f03500e24576
Grigoriy <grishkoshkarta1@gmail.com>
2022-12-19 22:05:34 +0200

    add foodpage

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -13,8 +13,12 @@ export class HomeComponent {
   foods:Food[] = [];
   constructor(private foodService:FoodService, activatedRoute: ActivatedRoute) {
     activatedRoute.params.subscribe((params) => {
+      console.log("params", params)
       if (params.searchTerm) {
         this.foods = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
+      } else if (params.tag) {
+        console.log(this.foods = this.foodService.getAllFoodsByTag(params.tag))
+        this.foods = this.foodService.getAllFoodsByTag(params.tag);
       } else  {
         this.foods = foodService.getAll();
       }
